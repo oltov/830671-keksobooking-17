3b54c83d7383acabb04e84aefa537aab32f5b14a
oltov <ururairurai@gmail.com>
2019-06-13 23:36:08 +0300

    функция для генерации меток объявлений

--- js/main.js
+++ js/main.js
@@ -1,8 +1,32 @@
-"use strict";
+'use strict';
 
 var numbersFileAvatars = ['01', '02', '03', '04', '05', '06', '07', '08'];
 var housingTypes = ['palace', 'flat', 'house', 'bungalo'];
 var locationPoints = function (min, max) {
   var point = Math.floor(min + Math.random() * (max + 1 - min));
   return point;
-}
+};
+
+var makeAdvert = function () {
+  var adverts = [];
+  for (var i = 0; i < 8; i++) {
+    adverts[i] = {
+      'autor': {
+        'avatar': 'img/avatars/user' + numbersFileAvatars[Math.floor(Math.random() * numbersFileAvatars.length)] + '.png'
+      },
+      'offer': {
+        'type': housingTypes[Math.floor(Math.random() * housingTypes.length)]
+      },
+      'location': {
+        'x': locationPoints(40, 1160),
+        'y': locationPoints(130, 630)
+      }
+    };
+  }
+  return adverts;
+};
+
+var markers = makeAdvert();
+
+document.querySelector('.map').classList.remove('map--faded');
+
