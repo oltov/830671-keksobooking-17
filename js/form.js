69e0687aee1710f9be98c4ac30507b0eea62ecbd
oltov <ururairurai@gmail.com>
2019-07-21 13:27:08 +0300

    доработка по колличеству гостей и комнат

--- js/form.js
+++ js/form.js
@@ -5,10 +5,8 @@
   var roomNumber = form.querySelector('#room_number');
   var roomCapacity = form.querySelector('#capacity');
   var buttonSubmit = form.querySelector('.ad-form__submit');
-  var title = form.querySelector('#title');
 
-
-  var checkCapacity = function (evt) {
+  var checkCapacity = function () {
     window.console.log(roomNumber.value);
     window.console.log(roomCapacity.value);
 
@@ -16,38 +14,40 @@
       if (roomCapacity.value !== '1') {
         roomCapacity.setCustomValidity('Выберете колличество гостей 1');
         window.console.log(roomNumber.value);
-        // evt.preventDefault();
-      } else {roomCapacity.setCustomValidity('')};
-    };
+      } else {
+        roomCapacity.setCustomValidity('');
+      }
+    }
+
     if (roomNumber.value === '2') {
-      if (roomCapacity.value !== '1' && '2') {
+      if (roomCapacity.value !== '2' && roomCapacity.value !== '1') {
         roomCapacity.setCustomValidity('Выберете колличество гостей 1 или 2');
-        window.console.log(roomNumber.value);
-        // evt.preventDefault();
-      } else {roomCapacity.setCustomValidity('')};
-    };
+      } else {
+        roomCapacity.setCustomValidity('');
+      }
+    }
+
     if (roomNumber.value === '3') {
-      if (roomCapacity.value !== '1' && '2' && '3') {
+      if (roomCapacity.value !== '3' && roomCapacity.value !== '2' && roomCapacity.value !== '1') {
         roomCapacity.setCustomValidity('Выберете колличество гостей 1, 2 или 3');
-        window.console.log(roomNumber.value);
-        // evt.preventDefault();
-      } else {roomCapacity.setCustomValidity('')};
-    };
+      } else {
+        roomCapacity.setCustomValidity('');
+      }
+    }
+
     if (roomNumber.value === '100') {
       if (roomCapacity.value !== '0') {
         roomCapacity.setCustomValidity('В 100 комнатах живут только призраки');
-        window.console.log(roomNumber.value);
-        // evt.preventDefault();
-      } else {roomCapacity.setCustomValidity('')};
-    };
+      } else {
+        roomCapacity.setCustomValidity('');
+      }
+    }
 
   };
 
-  buttonSubmit.addEventListener('click', function (evt) {
-    // evt.preventDefault();
-    // window.console.log(roomCapacity);
+  buttonSubmit.addEventListener('click', function () {
 
-    checkCapacity(evt);
+    checkCapacity();
   });
 
 })();
